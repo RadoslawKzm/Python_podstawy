d41e40f74d10199f32ad0565faf78c704231ed3a
rdkkz <Bolszewo11>
2020-05-12 17:49:41 +0200

    zadanie

--- zajecia2.py
+++ zajecia2.py
@@ -1,5 +1,6 @@
 import time
 
+
 # tstart = time.time()
 # lst = []
 # for i in range(1_000_000):
@@ -13,9 +14,23 @@ import time
 # lst3 = [i for i in range(1_000_000)]
 # print(f"funkcja 2 >> {time.time() - tstart:0.4f}s")
 
+def choinka():
+    rows = 5
+    var = rows * 2 + 1
+    for row in range(rows):
+        print(f"{'*' * (row * 2 + 1):^{var}}")
+    return var
 
-for row in range(8):
-    print(f"{'*'*(row*2+1):^10}")
+
+test = choinka()
+
+
+def multiply(a, b):
+    var = a * b
+    return var
+
+
+wynik = multiply(1, 2)
 
 """
 0-1     *
@@ -23,4 +38,39 @@ for row in range(8):
 2-5   *****
 3-7  *******
 """
+"""
+0-1 *
+1-3 ***
+2-5 *****
+3-7 *******
+"""
 
+"""
+for row in range(3)
+row = 0
+stars = row * 2 + 1
+text = row * 2 + 1
+print >> |*|
+
+row = 1
+stars = row * 2 + 1 =   1 * 2 + 1 = 3
+text = row * 2 + 1 =   1 * 2 + 1 = 3
+text = |_ _ _|
+print >> |***|
+
+|_ _ _ _ _|  << * x1
+|_ _ _ _ _|  << * x3
+|_ _ _ _ _|  << * x5
+
+|_ _ * _ _|  << * x1
+|_ * * * _|  << * x3
+|* * * * *|  << * x5
+
+print(f"{'cokolwiek':^}")
+
+|cokolwiek| <>^
+|cokolwiek                       | <
+|                       cokolwiek| >
+|            cokolwiek           | ^
+
+"""
