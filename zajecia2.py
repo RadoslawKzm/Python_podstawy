93fce20d8e0f49a374f309b034dd914a25ab5342
rdkkz <Bolszewo11>
2020-05-12 16:26:15 +0200

    zadanie

--- zajecia2.py
+++ zajecia2.py
@@ -1,8 +1,26 @@
 import time
 
-lst = []
-for i in range(100):
-    lst.append(i)
+# tstart = time.time()
+# lst = []
+# for i in range(1_000_000):
+#     lst.append(i)
+# tend = time.time()
+# result = tend - tstart
+# print(f"funkcja 1 >> {result:0.4f}s")
 
-lst3 = [i for i in range(100)]
+
+# tstart = time.time()
+# lst3 = [i for i in range(1_000_000)]
+# print(f"funkcja 2 >> {time.time() - tstart:0.4f}s")
+
+
+for row in range(8):
+    print(f"{'*'*(row*2+1):^10}")
+
+"""
+0-1     *
+1-3    ***
+2-5   *****
+3-7  *******
+"""
 
